34e5d073b3baeb615345a2c260f23cbbb2271ecc
笹澤ようすけ <sasaicco@gmail.com>
2025-08-24 10:10:20 +0900

    削除

--- books/auto-trade-system/src/app/utils/db_utils.py
+++
@@ -1,9 +0,0 @@
-import chardet
-
-# ファイルをバイナリモードで読み込み、エンコーディングを検出
-def get_encoding(file_path):
-    with open(file_path, 'rb') as file:
-        raw_data = file.read()
-        result = chardet.detect(raw_data)
-        encoding = result['encoding']
-        return encoding
